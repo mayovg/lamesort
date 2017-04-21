849697138eabd31fd3d195b7097c147689d4ae28
Luis P. Mayo <pmayov@gmail.com>
2017-04-21 01:44:48 -0500

    escribiendo mucha basura

--- Quicksort.java
+++ Quicksort.java
@@ -6,7 +6,7 @@ import java.util.Arrays;
 public class Quicksort implements Serializable {
 
     private int [] elem; // Arreglo de elementos a ordernar
-    private int izq, der; //indices para los hilos
+    private int izq, i, der, d; //indices
     private int rango;
     private int num_elem; // Número de elementos a ordenar
     
@@ -18,16 +18,6 @@ public class Quicksort implements Serializable {
     private Quicksort(int izq, int der){
 	this.izq = izq;
 	this.der = der;
-    }
-
-    private static void swap(int x, int y){
-	int tmp = x;
-	x = y;
-	y = tmp;
-    }
-    
-    public static void main(String[] args){
-	
     }
 
     protected class QuicksortServer implements Runnable{
@@ -42,6 +32,7 @@ public class Quicksort implements Serializable {
 	    qst.start();
 	}
 
+	/*Hace todo el trabajo*/
 	@Override
 	public void run(){
 	    try {
@@ -50,70 +41,77 @@ public class Quicksort implements Serializable {
 		er = new ExtendedRendezvous<Quicksort>(socket);
 		qs = er.getRequest();
 		int piv = qs.elem[qs.izq];
-		int i = qs.izq, d = qs.der;
+		qs.i = qs.izq;
+		qs.d = qs.der;
 		if (qs.der - qs.izq <= 0) return;
 		boolean done = false;
 		while (!done){
-		    if (qs.elem[i+1] > piv){
-			while(d > i+1 && qs.elem[d] > piv) d--;
-			if (d > i+1){
-			    i++;
-			    swap(d,i);
-			    done = (i >= d-1);
+		    if (qs.elem[(qs.i)+1] > piv){
+			while(qs.d > (qs.i+1) && qs.elem[qs.d] > piv) (qs.d)--;
+			if (qs.d > (qs.i)+1){
+			    (qs.i)++;
+			    swap(qs.d,qs.i);
+			    done = (qs.i >= (qs.d)-1);
 			} else done = true;
 		    } else {
-			i++;
-			done = i >= d;
+			(qs.i)++;
+			done = qs.i >= qs.d;
 		    }
-		    swap(qs.elem[izq], qs.elem[i]);
+		    swap(qs.elem[qs.izq], qs.elem[qs.i]);
 		}
+		er.response(qs);
 	    } catch(Exception e) {e.printStackTrace();}
 	}
     }
 
     protected class QuicksortClient implements Runnable{
-	
+	Quicksort qs;
 	public QuicksortClient(int izq, int der){
-	    Quicksort qs = new Quicksort(izq,der);
+	    this.qs = new Quicksort(izq,der);
 	    Thread qst = new Thread(this);
 	    qst.start();
-	    //return qst;
 	}
 
 	@Override
 	public void run(){
 	    try{
-	    Socket socket = new Socket("localhost", 8080);
-	    ExtendedRendezvous<Quicksort> er = new ExtendedRendezvous<Quicksort>(socket);
+		Socket socket = new Socket("localhost", 8080);
+		ExtendedRendezvous<Quicksort> er = new ExtendedRendezvous<Quicksort>(socket);
+		Quicksort aux = er.requestAndAwaitReply(this.qs);
+		if((aux.der - ((aux.i)+1) > 0)){
+		    Quicksort qsd = er.requestAndAwaitReply(new Quicksort((aux.i)+1, aux.der));	
+		}
+		if (((aux.i)-1)-aux.izq > 0){
+		    Quicksort qsi = er.requestAndAwaitReply(new Quicksort(aux.izq, (aux.i)-1));
+		}	    
 	    } catch (Exception e){e.printStackTrace();}
 	}
     }
-	//@Override
-	public void run(){	
-	    int piv = elem[izq]; //pivote
-	    int i = izq,
-		d = der;
-	    Thread qsi = null, // hilo de recursión por izquierda
-		qsd = null; // hillo de recursión por derecha
-	    if (der - izq <= 0) return; // nothing to do here
-	    boolean done = false;
-	    while (!done){
-		if (elem[i+1] > piv) {
-		    while (d > i+1 && elem[d] > piv) d--;
-		    if (d > i+1){
-			i++;
-			swap(d,i);
-			done = (i >= d-1);
-		    } else done = true;
-		} else {
-		    i++;
-		    done = i >= d;
+    
+    private static void swap(int x, int y){
+	int tmp = x;
+	x = y;
+	y = tmp;
+    }
+    
+    public static void main(String[] args){
+	int num_elem = 10000; // número de elementos a ordenar
+	int rango = (int) Math.pow(2,18); // rango de los elementos en el arreglo
+	System.out.println("Uso: java Quicksort -nNumeroDeElementos -rRangoDeValores");
+	for (int n = 0; n < args.length; n++){
+	    try{
+		if (args[n].startsWith("-n")){
+		    num_elem = Integer.parseInt(args[n].substring(1));
+		    if (num_elem <= 0){
+			num_elem = 1000;
+		    }
+		} else if (args[n].startsWith("-r")){
+		    rango = Integer.parseInt(args[n].substring(1));
+		    if(rango <= 0) rango = 10000;
 		}
-		swap(elem[izq], elem[i]);
-	    }
-	    //if(der-(i+1) > 0) qsd; //recursión por derecha
-	    //if((i-1)-izq > 0) qsi; //recursión por izquierda
-
-	    /*Comunicación*/
+	    } catch(Exception some_exception){some_exception.printStackTrace();}
 	}
+	
+    }
+
 }    
